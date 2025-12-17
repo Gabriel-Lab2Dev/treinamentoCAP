aea0374592d00c2d01b3077f4cad542d45913fe9
Gabriel Souza <gabriel.souza@lab2dev.com>
2025-12-17 20:38:00 +0000

    Modulo5 - exemplos

--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -109,9 +109,43 @@ class CatalogService extends cds.ApplicationService {
         req.reject(400, "Parâmetro 'user_ID' inválido");
       }
 
-      const tasks = await SELECT.from(Tasks).where({ user_ID: userID,concluida:0 });
+      const tasks = await SELECT.from(Tasks).where({
+        user_ID: userID,
+        concluida: 0,
+      });
 
       return tasks;
+    });
+
+    // MODULO 5
+
+    this.before("CREATE", Games, (req) => {
+      const { title } = req.data;
+      if (title?.length < 5) {
+        return req.error(400, "Game title should be bigger than 5 characters");
+      }
+    });
+
+    this.after("CREATE", Games, (results, req) => {
+      console.log("Foram criados os games");
+      console.log(results);
+    });
+
+    const { Games: GamesEntity } = cds.entities("com.lab2dev.gameshop.db");
+
+    this.on("CREATE", Games, async (req) => {
+      const game = req.data;
+      const existingGame = await SELECT.one
+        .from(GamesEntity)
+        .where({ title: game.title });
+
+      if (existingGame) {
+        return req.error(400, "Já tem um jogo com o mesmo nome");
+      }
+
+      await INSERT.into(GamesEntity, game);
+      game.title = "Outro Titulo";
+      return game;
     });
 
     return super.init();

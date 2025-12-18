289926729282a68dfd7100df9e3d428f8b4fe8fa
Gabriel Souza <gabriel.souza@lab2dev.com>
2025-12-18 18:17:31 +0000

    modulo5 - exemplos

--- srv/catalog-service.cds
+++ srv/catalog-service.cds
@@ -9,15 +9,7 @@ service CatalogService {
     entity Developers as projection on db.Developers;
 
     @readonly
-    entity Games      as projection on db.Games;
-
-    @readonly
-    entity GamesName  as
-        select
-            ID,
-            title,
-            developer.name as developer_name
-        from Games
+    entity Games as projection on db.Games;
 
     @readonly
     entity Tasks      as projection on db.Tasks;
@@ -25,23 +17,7 @@ service CatalogService {
     @readonly
     entity Users      as projection on db.Users;
 
-    action   stockRemoval(id : UUID, amount : Integer)  returns Games;
-    function getGamesStock(stock : Integer @mandatory ) returns array of Games;
 
-    action newTask(
-        titulo : String,
-        descricao : String,
-        concluida : Integer,
-        user_ID : UUID)                    
-        returns Tasks;
 
-    action modifyTask(
-        ID : UUID,
-        titulo : String,
-        descricao : String,
-        concluida : Integer,
-        user_ID : UUID
-        ) returns Tasks
-
-    function listUserTask(userID: UUID) returns array of Tasks
+    
 }
--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -1,123 +1,9 @@
 const cds = require("@sap/cds");
 const { SELECT } = require("@sap/cds/lib/ql/cds-ql");
+const { Games: GamesEntity } = cds.entities("com.lab2dev.gameshop.db");
 
 class CatalogService extends cds.ApplicationService {
   async init() {
-    this.on("stockRemoval", async (req) => {
-      const { id, amount } = req.data;
-      const { Games } = cds.entities;
-
-      // 1️⃣ Buscar o jogo
-      const game = await SELECT.one.from(Games).where({ ID: id });
-
-      // 2️⃣ Validar existência
-      if (!game) {
-        req.reject(404, "Jogo não encontrado");
-      }
-
-      // 3️⃣ Validar quantidade
-      if (amount <= 0) {
-        req.reject(400, "Quantidade inválida");
-      }
-
-      // 4️⃣ Atualizar estoque
-      const newStock = game.stock - amount;
-
-      await UPDATE(Games).set({ stock: newStock }).where({ ID: id });
-
-      // 5️⃣ Retornar jogo atualizado
-      return {
-        ID: game.ID,
-        title: game.title,
-        price: game.price,
-        stock: newStock,
-        releaseDate: game.releaseDate,
-        developer_ID: game.developer_ID,
-      };
-    });
-
-    this.on("getGamesStock", async (req) => {
-      const { stock } = req.data; // vem da query
-      const { Games } = cds.entities;
-
-      // Validação básica
-      if (stock == null || stock < 0) {
-        req.reject(400, "Parâmetro 'stock' inválido");
-      }
-
-      // Buscar jogos com estoque >= valor informado
-      const games = await SELECT.from(Games).where({ stock: { ">=": stock } });
-
-      return games;
-    });
-
-    this.on("newTask", async (req) => {
-      const { titulo, descricao, concluida, user_ID } = req.data;
-      const { Tasks } = cds.entities;
-
-      if (!titulo) {
-        req.reject(400, "Título é obrigatório");
-      }
-
-      if (concluida !== 0 && concluida !== 1) {
-        req.reject(400, "status de conclusão inválido");
-      }
-
-      if (!user_ID) {
-        req.reject(400, "Usuário Inválido");
-      }
-
-      const newTask = await INSERT.into(Tasks).entries({
-        titulo,
-        descricao,
-        concluida,
-        user_ID,
-      });
-
-      const createdTask = await SELECT.one
-        .from(Tasks)
-        .where({ titulo: titulo });
-
-      return createdTask;
-    });
-
-    this.on("modifyTask", async (req) => {
-      const { Tasks } = cds.entities;
-
-      const { ID, titulo, descricao, concluida, user_ID } = req.data;
-      let fieldsUpdate = [
-        ["titulo", titulo],
-        ["descricao", descricao],
-        ["concluida", concluida],
-        ["user_ID", user_ID],
-      ];
-
-      fieldsUpdate = Object.fromEntries(
-        fieldsUpdate.filter(([_, value]) => value)
-      );
-
-      await UPDATE(Tasks).set(fieldsUpdate).where({ ID });
-
-      return { ID: ID, fieldsUpdate };
-    });
-
-    this.on("listUserTask", async (req) => {
-      const { userID } = req.data;
-      const { Tasks } = cds.entities;
-
-      if (!userID) {
-        req.reject(400, "Parâmetro 'user_ID' inválido");
-      }
-
-      const tasks = await SELECT.from(Tasks).where({
-        user_ID: userID,
-        concluida: 0,
-      });
-
-      return tasks;
-    });
-
-    // MODULO 5
 
     this.before("CREATE", Games, (req) => {
       const { title } = req.data;
@@ -130,8 +16,6 @@ class CatalogService extends cds.ApplicationService {
       console.log("Foram criados os games");
       console.log(results);
     });
-
-    const { Games: GamesEntity } = cds.entities("com.lab2dev.gameshop.db");
 
     this.on("CREATE", Games, async (req) => {
       const game = req.data;
@@ -147,6 +31,22 @@ class CatalogService extends cds.ApplicationService {
       game.title = "Outro Titulo";
       return game;
     });
+
+    //ATVS MODULO 5
+
+    //ATV1
+
+    // this.before("READ", Games, (req) => {
+    //   const { title } = req.data;
+    //   if (title?.length < 5) {
+    //     return req.error(400, "Game title should be bigger than 5 characters");
+    //   }
+    // });
+
+    // this.after("CREATE", Games, (results, req) => {
+    //   console.log("Foram criados os games");
+    //   console.log(results);
+    // });
 
     return super.init();
   }
